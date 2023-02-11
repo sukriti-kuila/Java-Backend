c66308c95f88f51744a128827c8fbce05e57871b
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2023-02-11 21:30:57 +0530

    Add files via upload

---
+++ JDBC_Learning/src/Demo.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+public class Demo {
+	
+	public static void main(String[] args) throws Exception {
+		String url="jdbc:mysql://localhost:3306/sukritidb";
+		String uname="root";
+		String pass="Telusko@123";
+		String query = "select sname from student where rollno=2";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url,uname,pass);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		
+		// fetching data
+		rs.next();
+		String name = rs.getString("sname");
+		System.out.println(name);
+		
+		st.close();
+		con.close();
+	}
+
+}
