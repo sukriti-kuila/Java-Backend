06ae8d20102a7d62993f1e1f65629e12a8417a08
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2023-02-14 22:36:08 +0530

    Update AddServlet.java

--- Servlet & JSP Learning/1/src/main/java/com/servlet/AddServlet.java
+++ Servlet & JSP Learning/1/src/main/java/com/servlet/AddServlet.java
@@ -20,3 +20,20 @@ public class AddServlet extends HttpServlet
 		
 	}
 }
+
+// another process
+// import java.io.*;
+// import javax.servlet.*;
+// import javax.servlet.http.*;
+
+// public class HelloWorldServlet extends HttpServlet {
+//   public void doGet(HttpServletRequest request, HttpServletResponse response) 
+//     throws ServletException, IOException {
+//     response.setContentType("text/html");
+//     PrintWriter out = response.getWriter();
+//     out.println("<html><body>");
+//     out.println("<h1>Hello World!</h1>");
+//     out.println("</body></html>");
+//   }
+// }
+
