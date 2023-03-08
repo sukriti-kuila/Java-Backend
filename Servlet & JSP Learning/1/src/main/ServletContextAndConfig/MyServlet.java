b7b6ea47de064740bd3e2f3161555928b6817545
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2023-03-08 14:06:02 +0530

    Add files via upload

---
+++ Servlet & JSP Learning/1/src/main/ServletContextAndConfig/MyServlet.java
@@ -0,0 +1,19 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		PrintWriter out = res.getWriter();
+		ServletContext ctx = getServletContext();
+		String n = ctx.getInitParameter("Name"); // "name" value is mentioned in web.xml
+		out.print("Hi "+ n);
+	}
+}
